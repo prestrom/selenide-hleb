e6ab60ce2cfb4f30d3f077c8eebf2bd4f9e544aa
prestrom <hlebprestrom@gmail.com>
2021-02-19 13:57:29 +0300

    Edit cardfile chages

--- src/test/java/botest.java
+++ src/test/java/botest.java
@@ -1,22 +1,29 @@
 import com.codeborne.selenide.Condition;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selectors;
+import helpers.Util;
+import org.junit.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 
-import static com.codeborne.selenide.Selectors.byAttribute;
+import java.awt.*;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selectors.byValue;
+import static com.codeborne.selenide.Condition.*;
 
 public class botest {
+
+private String corpName, corpStatus;
+@BeforeClass
+public static void beforeClass() {
     String login = "ce";
     String password = "password";
-    boolean result;
-
-@Before
-public void loggedin() {
- open("https://qa-bo.statecap.co/backoffice/login");
+    Configuration.baseUrl="https://qa-bo.statecap.co/backoffice";
+    Configuration.holdBrowserOpen = true;
+ open("/login");
  $(By.id("userName")).setValue(login);
  $(By.id("password")).setValue(password);
  $(byValue("Login")).click();
@@ -24,8 +31,50 @@ public void loggedin() {
 
 @Test
 public void mainPageTest() {
-    $(By.id("logo")).should(Condition.exist);
+    $(By.id("logo")).should(exist);
 
+}
+
+@Test
+    public void corpCardEditTest() {
+    open("/corporation/maintenance");
+    $(By.id("lookBy")).selectOption("Corporation Number");
+    $(By.id("criteria")).selectOption("Any Part of Field");
+    $(By.id("searchValue")).setValue("1");
+    $(By.name("findBtn")).click(); // search for companies list
+        Util.waitForAjaxCompleted();
+    ElementsCollection allLinks = $$(By.className("editCorporation"))
+            .excludeWith(attribute("title", "Edit"))
+            .excludeWith(attribute("title", "New")); // collect all company name links into list
+
+
+    Util.getRandomElementFromList(allLinks).click(); // click random company list
+    Util.waitForAjaxCompleted();
+    $(By.id("corporationEditBlock")).is(visible);
+    corpName = $(By.name("corpname")).getAttribute("value")+" Selenide Test";
+    $(By.name("corpname")).setValue(corpName);
+    $(By.name("corpno")).setValue("0123456");
+    Util.getRandomOption($(By.name("incstate")));
+    $(By.name("incdate")).setValue(Util.getCurrentDate());
+    $(By.name("corpinfo")).setValue("Selenide Test Corp info");
+    Util.getRandomOption($(Selectors.byAttribute("name*", "statute")));
+    $(Selectors.byAttribute("id*", "updateLastStatusReport")).click();
+    $(By.name("corpstat")).setValue("Corp Status Selenide Test");
+    $(By.name("agent")).setValue("Agent Selenide Test");
+    $(By.name("agentAddress")).setValue("Agent address Selenide Test");
+    $(By.name("officersDirectors")).setValue("Officers & Directors Selenide Test");
+    if ($(By.id("ordBlock")).is(hidden)) {
+        $(By.name("tcyes")).click();
+    }
+    $(By.name("orddate")).setValue(Util.getCurrentDate());
+    $(By.name("ordcust")).setValue("ord customer Selenide Test");
+    $(Selectors.byAttribute("id*", "updateLastTaxReport")).click();
+    $(By.name("taxInformation")).setValue("Tax Info Selenide Test");
+    $(By.name("miscnotes")).setValue("testNote for Selenide Test");
+    $(By.name("save")).click();
+    Util.waitForAjaxCompleted();
+    $(By.className("t"+"-"+"message"+"-"+"container")).should(appear)
+            .shouldHave(text("The corporation " + corpName + " was edited.")).shouldNot(visible);
 }
 
 
--- src/test/java/helpers/Util.java
+++ src/test/java/helpers/Util.java
@@ -1,4 +1,47 @@
 package helpers;
 
-public class Util {
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.$;
+public final class Util {
+    public static void waitForAjaxCompleted() {
+        Configuration.timeout = 45000;
+        sleep(1000);
+        $(By.className("zone-ajax-overlay")).should(Condition.disappear);
+    }
+
+    public static  <T> T getRandomElementFromList(List<? extends T> list) {
+        Random random = new Random();
+        return list.get(random.nextInt(list.size()));
+    }
+
+    public static int getRandomInt (int i) {
+        Random rand  = new Random();
+        return rand.nextInt(i);
+    }
+
+
+    public static void getRandomOption (SelenideElement element) {
+        Select select = new Select(element);
+        int optionSize = select.getOptions().size();
+        Random rand = new Random();
+        element.selectOption(rand.nextInt(optionSize));
+
+    }
+    public static String getCurrentDate() {
+        Date date = new Date();
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        return dateFormat.format(date);
+    }
 }
